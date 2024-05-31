7e96cea45e6275998e04a1af97bcee022621901e
Farasat Khan <farasat.khan@jintech.com>
2024-05-31 15:21:37 +0500

    simple counter

--- DemoAppFlyout/DemoAppFlyout/Views/CoffeeEquipmentPage.xaml.cs
+++ DemoAppFlyout/DemoAppFlyout/Views/CoffeeEquipmentPage.xaml.cs
@@ -12,9 +12,16 @@ namespace DemoAppFlyout.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoffeeEquipmentPage : ContentPage
     {
+        int count = 0;
+
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
+        }
+
+        private void onButtonClicked(object sender, EventArgs e) {
+            count++;
+            CountLabel.Text = $"You Clicked {count} times.";
         }
     }
 }
