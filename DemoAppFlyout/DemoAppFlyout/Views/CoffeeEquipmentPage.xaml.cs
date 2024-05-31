619f6d6323759267384555768b4f6f57c40d0203
Farasat Khan <farasat.khan@jintech.com>
2024-05-31 16:45:43 +0500

    added view models

--- DemoAppFlyout/DemoAppFlyout/Views/CoffeeEquipmentPage.xaml.cs
+++ DemoAppFlyout/DemoAppFlyout/Views/CoffeeEquipmentPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace DemoAppFlyout.Views
@@ -12,35 +6,9 @@ namespace DemoAppFlyout.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CoffeeEquipmentPage : ContentPage
     {
-        
-
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
-            IncreaseCount = new Command(OnIncrease);
-            BindingContext = this;
-        }
-
-        public ICommand IncreaseCount { get;  }
-
-        int count = 0;
-        string countDisplay = "Click Me!";
-        public string CountDisplay
-        {
-            get => countDisplay;
-            set
-            {
-                if (value == countDisplay) return;
-
-                countDisplay = value;
-                OnPropertyChanged();
-            }
-        }
-
-        void OnIncrease()
-        {
-            count++;
-            CountDisplay = $"You Clicked {count} times.";
         }
     }
 }
