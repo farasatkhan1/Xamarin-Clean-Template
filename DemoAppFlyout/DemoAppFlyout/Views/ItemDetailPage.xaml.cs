6a4114805df4c7c2c05a3a0b69f518c78e4e3c3f
Farasat Khan <farasat.khan@jintech.com>
2024-05-31 11:53:35 +0500

    initial demo flyout commit

---
+++ DemoAppFlyout/DemoAppFlyout/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DemoAppFlyout.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DemoAppFlyout.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
